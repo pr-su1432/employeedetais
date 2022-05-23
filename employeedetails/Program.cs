94fcfea0d9e9f89d654b51bfaceeb7d052346acc
[prasanna] <prasanna>
2022-05-23 18:38:57 +0530

    [prasanna]: calculating employee wages for multiple companies

---
+++ employeedetails/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+employeedetails.employeedetails value = new employeedetails.employeedetails();
+value.calculateTotalEmpWage("Dmart",20,500,60);
+value.calculateTotalEmpWage("Reliance", 35, 700, 50);
